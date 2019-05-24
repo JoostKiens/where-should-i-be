807fe4cc84ebeeebc1d6b03d3b11726f9c0a9c8e
Joost Kiens <me@joostkiens.com>
2019-05-24 13:42:07 +0200

    extract magic numbers

--- src/partials/Toucher.js
+++ src/partials/Toucher.js
@@ -1,6 +1,13 @@
 import { useEffect, useRef, useState } from 'react'
 import { useStateValue } from '/machinery/state'
+import { clamp } from '/utils/clamp'
 const Hammer = typeof window !== 'undefined' ? require('hammerjs') : undefined
+
+const ACCELRATION = 0.95
+const PAN_FACTOR = 0.00004
+const MAX_PAN_VELOCITY = 200
+const SWIPE_FACTOR = 0.01
+const BRAKE_VELOCITY = 0.02
 
 export const Toucher = ({ style }) => {
   const [, dispatch] = useStateValue()
@@ -15,8 +22,8 @@ export const Toucher = ({ style }) => {
 
   useEffect(() => {
     function handlePan({ deltaX }) {
-      // @TODO clamp deltaX
-      dispatch({ type: 'incrementArc', value: deltaX * 0.0001 })
+      const velocity = clamp(-MAX_PAN_VELOCITY, MAX_PAN_VELOCITY, deltaX)
+      dispatch({ type: 'incrementArc', value: velocity * PAN_FACTOR })
       dispatch({ type: 'snapTo' })
     }
 
@@ -24,13 +31,15 @@ export const Toucher = ({ style }) => {
       updateVelocity(velocityX)
     }
 
-    function updateVelocity(vel) {
+    function updateVelocity(velocity) {
       let id
-      if (Math.abs(vel) > 0.02) {
-        dispatch({ type: 'incrementArc', value: vel * 0.01 })
+      if (Math.abs(velocity) > BRAKE_VELOCITY) {
+        dispatch({ type: 'incrementArc', value: velocity * SWIPE_FACTOR })
         dispatch({ type: 'isPanning', value: true })
         dispatch({ type: 'snapTo' })
-        id = window.requestAnimationFrame(() => updateVelocity(vel * 0.95))
+        id = window.requestAnimationFrame(() =>
+          updateVelocity(velocity * ACCELRATION)
+        )
       } else {
         window.cancelAnimationFrame(id)
         dispatch({ type: 'isPanning', value: false })
