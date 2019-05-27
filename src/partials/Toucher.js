b6d01aec0c2dc73386c1e6ede73efcafd51bf83d
Joost Kiens <me@joostkiens.com>
2019-05-27 12:29:21 +0200

    stop swiping on subsequent interactions

--- src/partials/Toucher.js
+++ src/partials/Toucher.js
@@ -5,17 +5,16 @@ const Hammer = typeof window !== 'undefined' ? require('hammerjs') : undefined
 
 const ACCELERATION = 0.975
 const PAN_FACTOR = 0.00004
-const MAX_PAN_VELOCITY = 200
+const MAX_PAN_VELOCITY = 100
 const SWIPE_FACTOR = 0.01
-const BRAKE_VELOCITY = 0.07
+const MIN_VELOCITY = 0.07
 
-// @TODO cancel animation on touch, at least the swipe animation
-// how? use a raf hook?
-// @TODO try to swipe non-discreet, until velocity is lower than n
 export const Toucher = ({ style }) => {
   const [, dispatch] = useStateValue()
   const [mc, setMc] = useState(null)
+  const [velocity, setVelocity] = useState(false)
   const el = useRef(null)
+  const frameID = useRef(0)
 
   useEffect(() => {
     if (!mc && el.current) {
@@ -24,33 +23,24 @@ export const Toucher = ({ style }) => {
   }, [setMc, mc, el])
 
   useEffect(() => {
-    function handlePan({ deltaX }) {
+    const handlePan = ({ deltaX, isFinal }) => {
+      if (!isFinal) stop()
       const velocityX = clamp(-MAX_PAN_VELOCITY, MAX_PAN_VELOCITY, deltaX)
       dispatch({ type: 'incrementArc', value: velocityX * PAN_FACTOR })
       dispatch({ type: 'snapTo' })
     }
 
-    function handleSwipe({ velocityX }) {
-      updateVelocity(velocityX)
-    }
+    const handleSwipe = ({ velocityX }) => setVelocity(velocityX)
 
-    function updateVelocity(velocity) {
-      let id
-      if (Math.abs(velocity) > BRAKE_VELOCITY) {
-        dispatch({ type: 'incrementArc', value: velocity * SWIPE_FACTOR })
-        dispatch({ type: 'isPanning', value: true })
-        dispatch({ type: 'snapTo' })
-        id = window.requestAnimationFrame(() =>
-          updateVelocity(velocity * ACCELERATION)
-        )
-      } else {
-        window.cancelAnimationFrame(id)
-        dispatch({ type: 'isPanning', value: false })
-      }
+    const stop = () => {
+      setVelocity(0)
+      window.cancelAnimationFrame(frameID.current)
     }
 
     if (mc) {
       mc.on('pan', handlePan)
+      mc.on('press', stop)
+      mc.on('tap', stop)
       mc.on('swipe', handleSwipe)
     }
 
@@ -58,9 +48,25 @@ export const Toucher = ({ style }) => {
       if (mc) {
         mc.off('pan', handlePan)
         mc.off('pan', handleSwipe)
+        mc.off('press', stop)
+        mc.off('tap', stop)
       }
     }
   }, [mc, dispatch])
+
+  useEffect(() => {
+    if (Math.abs(velocity) > MIN_VELOCITY) {
+      dispatch({ type: 'incrementArc', value: velocity * SWIPE_FACTOR })
+      dispatch({ type: 'snapTo' })
+      frameID.current = window.requestAnimationFrame(() =>
+        setVelocity(velocity * ACCELERATION)
+      )
+    } else {
+      setVelocity(0)
+      dispatch({ type: 'snapTo' })
+      window.cancelAnimationFrame(frameID.current)
+    }
+  }, [dispatch, velocity])
 
   return <div style={style} ref={el} />
 }
