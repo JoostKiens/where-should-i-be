e8a9f2b322e29fdcf431d55c17b9ae067d1aceff
Joost Kiens <me@joostkiens.com>
2019-05-29 16:29:28 +0200

    simplify config

--- config/dev.js
+++ config/dev.js
@@ -1,10 +1,1 @@
-const { resolve } = require('path')
-
-module.exports = {
-  server: {
-    firebase: {
-      credentials: resolve(process.cwd(), 'config', 'credentials.json'),
-      databaseURL: 'https://where-should-we-live.firebaseio.com',
-    },
-  },
-}
+module.exports = {}
