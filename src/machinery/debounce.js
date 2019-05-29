747427f1dad716710d29c45170d8d271a3f72fe7
Joost Kiens <me@joostkiens.com>
2019-05-29 13:45:24 +0200

    remove unused file

--- src/machinery/debounce.js
+++
@@ -1,8 +0,0 @@
-export const debounce = (callback, wait = 200) => {
-  let timeout = null
-  return (...args) => {
-    const next = () => callback(...args)
-    clearTimeout(timeout)
-    timeout = setTimeout(next, wait)
-  }
-}
